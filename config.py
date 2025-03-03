163bd23c9a95e8d0a178378e8bc27d23de9da80f
sabit005 <138222152+sabit005@users.noreply.github.com>
2025-03-03 17:28:41 +0600

    Update config.py

--- config.py
+++ config.py
@@ -28,11 +28,11 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6673527661"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sabitsgaming1:<db_password>@cluster0.803f7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002392412187"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
