cce693f4a1ca9b262400a4ca15f8fdd6dfcbff07
sabit005 <138222152+sabit005@users.noreply.github.com>
2025-03-03 18:55:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from bot import Bot
 import asyncio
 
 if __name__ == "__main__":
-    bot = Bot()  
-    asyncio.run(bot.start())  # ✅ use_qr সরিয়ে দিলাম
+    bot = bot()  
+    asyncio.run(bot.start())  
 
 
 
