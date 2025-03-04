7d0e2356e1d037fb11471138b36d41731fd30447
sabit005 <138222152+sabit005@users.noreply.github.com>
2025-03-04 06:36:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from bot import bot
+from bot import Bot  # এখানে 'Bot' ইমপোর্ট করছি
 import asyncio
 
 if __name__ == "__main__":
-    bot = bot()  
-    asyncio.run(bot.start())  
-
-
-
+    bot = Bot()  # এখানে 'Bot' অবজেক্ট তৈরি করছি
+    asyncio.run(bot.start())
